21d2a3718d6bb87abdbab2deb09d307cbac81d14
zyme <lolrichyisswag@gmail.com>
2021-02-28 16:22:14 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	// "net/http"
+	// "os"
+	// "errors"
+	// "bufio"
+	"fmt"
+	// "bytes"
+)
+
+func main() {
+	fmt.Println("This is a work in progress.")
+}
+
+
+	// var url string
+	// var input string
+	// fmt.Print("Please input a url:  ")
+	// fmt.Scanln(&url)
+	// fmt.Printf("You said %v, are you sure?:   ", url)
+	// fmt.Scanln(&input)
+	// if input == "true" {
+	// 	fmt.Println("Ok, let me download this for you.")
+	// } else {
+	// 	fmt.Println("Lol! Get trolled")
+	// }
