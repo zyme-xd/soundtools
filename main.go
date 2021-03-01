603fe100f7a8cb536a8f961a146d05127b523794
zyme <lolrichyisswag@gmail.com>
2021-02-28 21:11:57 -0600

    It works now!

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"net/http"
 	"os"
-	// "errors"
-	// "bufio"
-	"fmt"
 	"regexp"
-	"bytes"
 	"strings"
 )
 
@@ -19,36 +18,56 @@ func main() {
 }
 
 func getIds(url string) []string {
-	response, err := http.Get(url)
-    if err != nil {
-        fmt.Printf("%s", err)
-        os.Exit(1)
-    } else {
-        defer response.Body.Close()
-        if err != nil {
-            fmt.Printf("%s", err)
-        }
+	response, err := http.Get(url) // Gets URL provided by user.
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	} else {
+		defer response.Body.Close()
+		if err != nil {
+			fmt.Printf("%s", err)
+		}
 	}
 	buf := new(bytes.Buffer)
-    buf.ReadFrom(response.Body)
-    resp := buf.String()
-	re := regexp.MustCompile(`/sound/[0-9]*/`)
+	buf.ReadFrom(response.Body)
+	resp := buf.String()
+	re := regexp.MustCompile(`/sound/[0-9]*/`) // Regex to get sound ID's from the page
 	results := re.FindAllString(resp, -1)
-	str := strings.Join(results,"")
-	ids := strings.Split(str, "/sound")
-	return(ids)
+	str := strings.Join(results, "")
+	ids := strings.Split(str, "/sound") // Splitting garbage, how fun
+	return (ids)
 }
 
 func download(url string) {
 	ids := getIds(url)
 	ids[0] = ids[len(ids)-1] // this basically just removes an empty object that the regex grabs
-	ids[len(ids)-1] = ""   
-	ids = ids[:len(ids)-1]   
+	ids[len(ids)-1] = ""
+	ids = ids[:len(ids)-1]
 	fmt.Printf("%q\n", ids)
 	i := 0
-	for i < len(ids){
-		fmt.Println("I am going to go insane")
-		fmt.Println(i)
+	for i < len(ids) {
+		// Get the data
+		resp, err := http.Get("https://www.sounds-resource.com/download" + ids[i]) // Download endpoint
+		re := regexp.MustCompile(`"([^"]*)"`)
+		joined := strings.Join(resp.Header.Values("Content-Disposition"), " ") // Gets file name
+		header := re.FindString(joined)
+		cleaned := strings.ReplaceAll(header, `"`, "")
+		if err != nil {
+			return
+		}
+		name := cleaned
+		defer resp.Body.Close()
+		out, err := os.Create(name)
+		if err != nil {
+			return
+		}
+		defer out.Close()
+		// Write the body to file
+		_, err = io.Copy(out, resp.Body)
+		if err != nil {
+			return
+		}
 		i++
 	}
+	fmt.Println("Completed. Check the root folder of where you installed this software.")
 }
